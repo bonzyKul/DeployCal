807e6a11ce8cc97daec8ebcd2614a1bf37017dd9
Alok Kulkarni <alokkuls@icloud.com>
2015-05-10 23:25:44 +0530

    updated dist

--- public/dist/application.js
+++ public/dist/application.js
@@ -4,7 +4,7 @@
 var ApplicationConfiguration = (function() {
 	// Init module configuration options
 	var applicationModuleName = 'deploycal';
-	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate', 'ngAria', 'ngTouch',  'ngSanitize', 'ui.router', 'ui.bootstrap', 'ui.utils', 'ngMaterial'];
+	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate', 'ngAria', 'ngTouch',  'ngSanitize', 'ui.router', 'ui.bootstrap', 'ui.utils', 'ngMaterial', 'colorpicker.module'];
 
 	// Add a new vertical module
 	var registerModule = function(moduleName, dependencies) {
@@ -48,6 +48,10 @@ angular.element(document).ready(function() {
 ApplicationConfiguration.registerModule('core');
 'use strict';
 
+// Use applicaion configuration module to register a new module
+ApplicationConfiguration.registerModule('eventsdata');
+'use strict';
+
 // Use Applicaion configuration module to register a new module
 ApplicationConfiguration.registerModule('users');
 'use strict';
@@ -87,8 +91,8 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication', '$modal',
-	function($scope, Authentication, $modal) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', '$modal', 'Eventsdata', '$http',
+	function($scope, Authentication, $modal, Eventsdata, $http) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
 
@@ -96,11 +100,11 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
         //var startDate;
         //var endDate;
 
-        //$('#startDateTimepicker').datetimepicker();
-        //$('#endDateTimepicker').datetimepicker();
 
-        $scope.event = {title: "", startdate: "", endDate: ""};
 
+        $scope.event = {title: '', startdate: '', endDate: '', overlap: true, color: '', deleteEvent: '' };
+
+        $scope.events = [];
 
         var updateEvent =  function(calEvent) {
             if ($scope.authentication.user) {
@@ -120,43 +124,41 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
                                 startDate: calEvent.start,
                                 endDate: calEvent.end
                             });
-                        },
-                        events: function() {
-                            console.log("hello from events");
-                            return $scope.event;
                         }
                     } // empty storage
                 };
 
-
-                //$scope.opts.resolve.item = function() {
-                //    return angular.copy(
-                //        {name: calEvent.title,
-                //         startDate: calEvent.start,
-                //         endDate: calEvent.end}
-                //    ); // pass name to resolve storage
-                //};
+                console.log(calEvent.id);
 
                 var modalInstance = $modal.open($scope.opts);
 
                 modalInstance.result.then(function(result){
-                    calEvent.title = result.title;
-                    calEvent.start = result.startDate;
-                    calEvent.end = result.endDate;
-                    $('#calendar').fullCalendar('updateEvent',calEvent,true);
+                    if(result.deleteEvent === 'Y'){
+                        console.log('delete pressed');
+                        var eventsdatumId = calEvent.id;
+
+                        $http.delete('/eventsData/' + eventsdatumId).success(function(status){
+                            console.log(eventsdatumId);
+                            $('#calendar').fullCalendar('removeEvents',eventsdatumId);
+                        }).error(function(errorResponse) {
+                           console.log(errorResponse);
+                        });
+                    } else {
+                        calEvent.title = result.title;
+                        calEvent.start = result.startDate;
+                        calEvent.end = result.endDate;
+                        $('#calendar').fullCalendar('updateEvent',calEvent,true);
+                    }
                 },function(){
                     //on cancel button press
                     console.log('Modal Closed');
+                }, function() {
+                    console.log('delete pressed');
                 });
             } else {
                 alert("Please sign in to Update an Event");
                 $('#calendar').fullCalendar('unselect');
             }
-            //var title = prompt('Event Title:');
-            //if(title){
-            //    calEvent.title = title;
-            //    calEvent.color = 'blue';
-            //}
         };
 
         var ModalInstanceCtrl = function($scope, $modalInstance, items) {
@@ -172,15 +174,119 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
                 $modalInstance.dismiss('cancel');
             };
 
-            //var updatedEvent = function(calEvent, item) {
-            //    calEvent.title = item.name;
-            //    calEvent.start = item.startDate;
-            //    calEvent.end = item.endDate;
-            //}
+            $scope.delete = function() {
+                $scope.event.deleteEvent = 'Y';
+                $modalInstance.close($scope.event);
+            }
+        };
+
+     var createEvent = function(start, end) {
+        if ($scope.authentication.user) {
+            $scope.opts = {
+                backdrop: true,
+                backdropClick: true,
+                dialogFade: false,
+                keyboard: true,
+                headerText: 'Add Event',
+                templateUrl : '/modules/eventsdata/views/create-eventsdatum.client.view.html',
+                controller : CreateModalInstanceCtrl,
+                resolve: {
+                    items: function() {
+                        return angular.copy({
+                            startDate: start,
+                            endDate: end
+                        });
+                    }
+                } // empty storage
+            };
+
+            var modalInstance = $modal.open($scope.opts);
+
+            modalInstance.result.then(function(result){
+                console.log(result);
+                var eventsdatum = new Eventsdata ({
+                    name: result.title,
+                    startDate: result.startDate,
+                    endDate: result.endDate,
+                    overlap: !result.eventFreeze,
+                    color: result.eventColor
+                });
+                $http.post('/eventsdata',eventsdatum).success(function(status){
+                    console.log(status);
+                    findOne(status._id);
+                }). error(function(errorResponse) {
+                    console.log(errorResponse);
+                    //alert('an unexpected error occured' + errorResponse);
+                });
+            },function(){
+                //on cancel button press
+                console.log('Modal Closed');
+            });
+        } else {
+            alert("Please sign in to Update an Event");
+            $('#calendar').fullCalendar('unselect');
+        }
+    };
+
+    var CreateModalInstanceCtrl = function($scope, $modalInstance, items) {
+
+        $scope.event = items;
+
+        $scope.openStart = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            $scope.openedStart = true;
+        };
+
+        $scope.openEnd = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            $scope.openedEnd = true;
         };
 
 
+
+        $scope.ok = function () {
+            //events();
+            $modalInstance.close($scope.event);
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    };
+
+var findOne = function(eventsdatumId) {
+    $http.get('/eventsdata/' + eventsdatumId).success(function(data) {
+        console.log(JSON.stringify(data));
+        console.log(data.length);
+        $scope.events = {id: data._id, title: data.name, start: data.startDate, end: data.endDate,overlap: data.overlap,color: data.color,allDay: false};
+        console.log($scope.events);
+        $('#calendar').fullCalendar('renderEvent', $scope.events, true); // stick? = true
+    }).error(function() {
+        alert('an unexpected error occured');
+    });
+}
+
+        //retrieve events
+var events = function() {
+            $http.get('/eventsdata').success(function(data) {
+                console.log(JSON.stringify(data));
+                console.log(data.length);
+                for(var i = 0; i < data.length; i++) {
+                    $scope.events[i] = {id: data[i]._id, title: data[i].name, start: data[i].startDate, end: data[i].endDate,overlap: data[i].overlap,color: data[i].color,allDay: false};
+                    console.log($scope.events[i]);
+                    $('#calendar').fullCalendar('renderEvent', $scope.events[i], true); // stick? = true
+                }
+            }).error(function() {
+               alert('an unexpected error occured');
+            });
+        };
+
         $scope.init = function() {
+            events();
             $('#calendar').fullCalendar({
                 header: {
                   left: 'prev,next today',
@@ -193,72 +299,9 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
                 weekends: true,
                 weekNumbers: true,
                 droppable: true,
-                select:addEvent,
+                select:createEvent,
                 eventClick: updateEvent,
-                eventLimit: true,
-                events: [
-                    {
-                        title: 'All Day Event',
-                        start: '2015-04-01'
-                    },
-                    {
-                        title: 'Long Event',
-                        start: '2015-04-07',
-                        end: '2015-04-10'
-                    },
-                    {
-                        id: 999,
-                        title: 'Repeating Event',
-                        start: '2015-04-09T16:00:00'
-                    },
-                    {
-                        id: 999,
-                        title: 'Repeating Event',
-                        start: '2015-04-16T16:00:00'
-                    },
-                    {
-                        title: 'Conference',
-                        start: '2015-04-11',
-                        end: '2015-04-13'
-                    },
-                    {
-                        title: 'Meeting',
-                        start: '2015-04-12T10:30:00',
-                        end: '2015-04-12T12:30:00'
-                    },
-                    {
-                        title: 'Lunch',
-                        start: '2015-04-12T12:00:00'
-                    },
-                    {
-                        title: 'Meeting',
-                        start: '2015-04-12T14:30:00'
-                    },
-                    {
-                        title: 'Happy Hour',
-                        start: '2015-04-12T17:30:00'
-                    },
-                    {
-                        title: 'Dinner',
-                        start: '2015-04-12T20:00:00'
-                    },
-                    {
-                        title: 'Birthday Party',
-                        start: '2015-04-13T07:00:00'
-                    },
-                    {
-                        title: 'Click for Google',
-                        url: 'http://google.com/',
-                        start: '2015-04-28'
-                    },
-                    {
-                        start: '2015-04-24',
-                        end: '2015-04-28',
-                        overlap: false,
-                        rendering: 'background',
-                        color: '#ff9f89'
-                    }
-                ]
+                eventLimit: true
             });
 
         };
@@ -275,6 +318,12 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
                        end: end
                    };
                    $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+                   $scope.events.push({
+                      title: title,
+                       start: start,
+                       end: end
+                   });
+
                }
                $('#calendar').fullCalendar('unselect');
            } else {
@@ -286,6 +335,21 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	}
 ]);
 
+'use strict';
+
+angular.module('core').factory('Coreclientservicejs', [
+	function() {
+		// Coreclientservicejs service logic
+		// ...
+
+		// Public API
+		return {
+			someMethod: function() {
+				return true;
+			}
+		};
+	}
+]);
 'use strict';
 
 //Menu service used for managing  menus
@@ -450,6 +514,137 @@ angular.module('core').service('Menus', [
 
 		//Adding the topbar menu
 		this.addMenu('topbar');
+	}
+]);
+'use strict';
+
+//Setting up route
+angular.module('eventsdata').config(['$stateProvider',
+	function($stateProvider) {
+		// Eventsdata state routing
+		$stateProvider.
+		state('listEventsdata', {
+			url: '/eventsdata',
+			templateUrl: 'modules/eventsdata/views/list-eventsdata.client.view.html'
+		}).
+		state('createEventsdatum', {
+			url: '/eventsdata/create',
+			templateUrl: 'modules/eventsdata/views/create-eventsdatum.client.view.html'
+		}).
+		state('viewEventsdatum', {
+			url: '/eventsdata/:eventsdatumId',
+			templateUrl: 'modules/eventsdata/views/view-eventsdatum.client.view.html'
+		}).
+		state('editEventsdatum', {
+			url: '/eventsdata/:eventsdatumId/edit',
+			templateUrl: 'modules/eventsdata/views/edit-eventsdatum.client.view.html'
+		});
+	}
+]);
+'use strict';
+
+// Eventsdata controller
+angular.module('eventsdata').controller('EventsdataController', ['$scope', '$stateParams', '$location', 'Authentication', 'Eventsdata',
+	function($scope, $stateParams, $location, Authentication, Eventsdata) {
+		$scope.authentication = Authentication;
+
+        $scope.openStart = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            $scope.openedStart = true;
+        };
+
+        $scope.openEnd = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            $scope.openedEnd = true;
+        };
+
+
+		// Create new Eventsdatum
+		$scope.create = function() {
+			// Create new Eventsdatum object
+			var eventsdatum = new Eventsdata ({
+				name: this.title,
+                startDate: this.startDate,
+                endDate: this.endDate,
+                overlap: this.eventFreeze,
+                color: this.eventColor
+			});
+
+            console.log(this.eventFreeze);
+
+			// Redirect after save
+			eventsdatum.$save(function(response) {
+				//$location.path('eventsdata/' + response._id);
+
+				// Clear form fields
+				$scope.title = '';
+                $scope.startDate = '';
+                $scope.endDate = '';
+                $scope.eventFreeze = '';
+                $scope.eventColor = '';
+
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+		// Remove existing Eventsdatum
+		$scope.remove = function(eventsdatum) {
+			if ( eventsdatum ) { 
+				eventsdatum.$remove();
+
+				for (var i in $scope.eventsdata) {
+					if ($scope.eventsdata [i] === eventsdatum) {
+						$scope.eventsdata.splice(i, 1);
+					}
+				}
+			} else {
+				$scope.eventsdatum.$remove(function() {
+					$location.path('eventsdata');
+				});
+			}
+		};
+
+		// Update existing Eventsdatum
+		$scope.update = function() {
+			var eventsdatum = $scope.eventsdatum;
+
+			eventsdatum.$update(function() {
+				$location.path('eventsdata/' + eventsdatum._id);
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+		// Find a list of Eventsdata
+		$scope.find = function() {
+			$scope.eventsdata = Eventsdata.query();
+		};
+
+		// Find existing Eventsdatum
+		$scope.findOne = function() {
+			$scope.eventsdatum = Eventsdata.get({ 
+				eventsdatumId: $stateParams.eventsdatumId
+			});
+		};
+	}
+]);
+
+'use strict';
+
+//Eventsdata service used to communicate Eventsdata REST endpoints
+angular.module('eventsdata').factory('Eventsdata', ['$resource',
+	function($resource) {
+		return $resource('eventsdata/:eventsdatumId', { eventsdatumId: '@_id'
+		}, {
+			update: {
+				method: 'PUT'
+			}
+		});
 	}
 ]);
 'use strict';
